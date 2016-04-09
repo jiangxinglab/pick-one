946298cc64c1c0331a56d650dc5d47fa3930ad24
jiangxing <jiangxing@xidian.com>
2016-04-09 18:23:02 +0800

    second commit

---
+++ src/main/java/com/erliang/pick/one/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.erliang.pick.one;
+
+/**
+ * @author jiangxing
+ * @project temp
+ * @date 4/9/16
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
---
+++ src/test/java/com/erliang/pick/one/HelloWorldTest.java
@@ -0,0 +1,9 @@
+package com.erliang.pick.one;
+
+/**
+ * @author jiangxing
+ * @project temp
+ * @date 4/9/16
+ */
+public class HelloWorldTest {
+}
